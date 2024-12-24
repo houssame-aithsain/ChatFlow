d889eaab9f8355ad9996d307115f3f3528e02b29
Houssame <hossame@inbox.ru>
2024-12-24 06:45:36 +0100

    adding front-end container

--- back-end/chatFlow/settings.py
+++ back-end/chatFlow/settings.py
@@ -33,6 +33,12 @@ ALLOWED_HOSTS = [
 
 # Application definition
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",  # Frontend origin
+    "http://localhost:5500",  # Alternative localhost origin
+]
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'user',
 ]
 
@@ -52,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'chatFlow.urls'
@@ -128,3 +136,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.User'
+
+CORS_ALLOW_ALL_ORIGINS = True
